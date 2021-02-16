64c26db03f4e0eec97dbe635a1d07a77927ff573
Matt Lemke <mattrlemke@gmail.com>
2021-02-16 16:25:26 -0600

    added inner HTML and appendChild to JS

--- assets/script.js
+++ assets/script.js
@@ -5,10 +5,27 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskHandler = function(event) {
 
     event.preventDefault();
-    
+
+    // Variables
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    // Create List Item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+  // create div to hold task info 
+  var taskInfoEl = document.createElement("div");
+
+  // Give Class Name
+  taskInfoEl.className = "task-info";
+
+  // Add HTML Content
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class ='take-type'>" + taskTypeInput + "</span>";
+
+    listItemEl.appendChild(taskInfoEl);
+
+    // Add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
   };
 
